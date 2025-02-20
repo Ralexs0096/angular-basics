253cf45e05e102d1dfc07f57379923faa62a397d
Alexs Ruiz <ralexs.acu@gmail.com>
2025-02-20 17:00:58 -0600

    Add a new character

--- src/app/pages/dragonball/dragonball-page.component.ts
+++ src/app/pages/dragonball/dragonball-page.component.ts
@@ -1,32 +1,35 @@
 import { Component, signal } from '@angular/core';
 
 interface Characters {
-  id: number
-  name: string
-  power: number
+  id: number;
+  name: string;
+  power: number;
 }
 
 @Component({
-  templateUrl: './dragonball-page.component.html'
+  templateUrl: './dragonball-page.component.html',
 })
 export class DragonballPageComponent {
   characters = signal<Characters[]>([
     {
       id: 1,
       name: 'Goku',
-      power: 9001
+      power: 9001,
     },
     {
       id: 2,
       name: 'Vegeta',
-      power: 8000
+      power: 8000,
     },
     {
       id: 3,
       name: 'Piccolo',
-      power: 3000
-    }
-  ])
-
-
+      power: 3000,
+    },
+    {
+      id: 4,
+      name: 'Yamcha',
+      power: 500,
+    },
+  ]);
 }
