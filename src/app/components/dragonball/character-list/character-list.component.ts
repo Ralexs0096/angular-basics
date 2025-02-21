0544e0cf3addc77826932add9317c23587aa6cdd
Alexs Ruiz <ralexs.acu@gmail.com>
2025-02-21 11:15:09 -0600

    Use character-list component and provide the characters array as input

---
+++ src/app/components/dragonball/character-list/character-list.component.ts
@@ -0,0 +1,10 @@
+import { Component, input } from '@angular/core';
+import type { Characters } from '../../../interfaces/character.interface';
+
+@Component({
+  selector: 'dragonball-character-list',
+  templateUrl: './character-list.component.html',
+})
+export class CharacterListComponent {
+  characters = input.required<Characters[]>();
+}
